a134ddedc89a82c98564c642e3823355ee39c3b4
vitor <vitor_oliveira_20@hotmail.com>
2025-05-25 17:52:02 +0100

    var_dump function

---
+++ 04-var_dump/var_dump.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <pre><?php
+
+        var_dump('Hello from a var_dump');
+        var_dump('ß');
+
+        var_dump(123);
+        var_dump(42.42);
+
+        var_dump('4' + 5);
+
+        $message = 'Hello World';
+        var_dump($message);
+
+        $message2 = 'Hello World 2025';
+        var_dump($message2);
+
+    ?></pre>
+</body>
+</html>
